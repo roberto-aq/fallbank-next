9bcee4961cc33f6de656bc7168cc4a5ae29704b2
roberto-aq <baxro2000@gmail.com>
2024-06-30 00:02:03 -0500

    Implementado el layout completo

---
+++ app/(root)/my-banks/page.tsx
@@ -0,0 +1,7 @@
+export default function MyBanksPage() {
+	return (
+		<div>
+			<h1>Hello Page</h1>
+		</div>
+	);
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,42 @@
+import { HeaderBox } from '@/components/HeaderBox';
+import { RightSidebar } from '@/components/RightSidebar';
+import { TotalBalanceBox } from '@/components/TotalBalanceBox';
+
 export default function HomePage() {
+	const loggedIn = {
+		firstName: 'Roberto',
+		lastName: 'Andrade',
+		email: 'baxro2000@gmail.com',
+	};
+
 	return (
-		<div>
-			<h1>Hello Page</h1>
-		</div>
+		<section className='home'>
+			<div className='home-content'>
+				<header className='home-header'>
+					<HeaderBox
+						type='greeting'
+						title='Welcome'
+						user={loggedIn?.firstName || 'Guest'}
+						subtext='Access and manage your account and transactions efficiently'
+					/>
+
+					<TotalBalanceBox
+						accounts={[]}
+						totalBanks={1}
+						totalCurrentBalance={1250.35}
+					/>
+				</header>
+				RECENT TRANSACTIONS
+			</div>
+
+			<RightSidebar
+				user={loggedIn}
+				transactions={[]}
+				banks={[
+					{ currentBalance: 230.45 },
+					{ currentBalance: 1256.4 },
+				]}
+			/>
+		</section>
 	);
 }
